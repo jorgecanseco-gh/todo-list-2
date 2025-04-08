bb48f296647f700e8cf8f7ac8cc93f84867d4da5
Jorge.C <jorgecansecoreal@gmail.com>
2025-04-08 17:26:41 +0200

    Add styles to InputBar

--- src/Components/InputBar/InputBar.js
+++ src/Components/InputBar/InputBar.js
@@ -5,14 +5,18 @@ import React from 'react';
 function InputBar(props) {
     const {inputValue, setInputValue, handleAddTask} = props;
     return (
-        <div>
+        <div className={styles.inputBar}>
             <input
             type='text'
             placeholder='Add a new task...'
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
+            className={styles.inputField}
             />
-            <button onClick={handleAddTask}>
+            <button 
+            onClick={handleAddTask}
+            className={styles.addButton}
+            >
                 Add
             </button>
         </div>
